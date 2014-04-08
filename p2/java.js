342e79f434fb27062050af5ba544bdba7b069333
Tomar <timemachinez@tmm.risd.edu>
2014-04-08 13:19:21 -0400

    adding pj 2 and link

---
+++ p2/java.js
@@ -0,0 +1,26 @@
+function RL(){
+	var page = Math.round(Math.random()*9)
+	var links = new Array()
+	links[0]="123.com"
+	links[1]="123.com"
+	links[2]=""
+	links[3]=""
+	links[4]=""
+	links[5]=""
+	links[6]=""
+	links[7]=""
+	links[8]=""
+	links[9]=""
+	links[10]=""
+	links[11]=""
+	links[12]=""
+	links[13]=""
+	links[14]=""
+	links[15]=""
+	links[16]=""
+	links[17]=""
+	links[18]=""
+	links[19]=""
+	links[20]=""
+    window.location=links[page]
+}
